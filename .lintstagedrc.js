c6136953fb551488f806268bb8a06e8706bc7368
Satoshi OHKI <roothybrid7@gmail.com>
2020-01-27 13:10:16 +0900

    Added new lint-staged rules

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,6 +3,7 @@ module.exports = {
     `swiftformat .`,
     `git add ios/*.swift`
   ],
+  '*.podspec': () => `yarn pod:lib:lint`,
   'src/*.{ts,tsx}': () => [
     `yarn lint`,
     `git add src/*.{ts,tsx}`
